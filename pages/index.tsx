e2c2f4545b6c92d0215a1df5d9efcf73471bc4b2
ObadaAbdAlkhaleq <83885520+ObadaAbdAlkhaleq@users.noreply.github.com>
2023-02-04 00:40:55 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Senior Project</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+      <main>
+        <div className="border-4 rounded-lg border-black w-28 flex justify-center h-28 items-center text-2xl">
+        <p>clear</p>
+        </div>
+      </main>
     </div>
   )
 }
