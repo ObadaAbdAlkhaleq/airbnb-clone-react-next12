73e95607bb9ddd5b5beb572547c4a575b25df47d
ObadaAbdAlkhaleq <83885520+ObadaAbdAlkhaleq@users.noreply.github.com>
2023-02-09 01:44:11 +0200

    Create LargeCards.js

---
+++ components/LargeCard.js
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+function LargeCard({ img, title, description, buttonText }) {
+    return (
+        <section className='relative py-16 cursor-pointer'>
+            <div className="relative h-96 min-w-[300px]">
+                <Image 
+                    className="rounded-2xl" 
+                    src={img} 
+                    fill 
+                    style={{ objectFit: "cover" }}
+                />
+            </div>
+            <div className=' left-12 z-50 absolute top-32'>
+                <h1 className=' w-64 break-normal text-4xl font-bold'>{title}</h1>
+                <p className=' text-lg font-medium'>{description}</p>
+                <button className=' bg-gray-900 text-white px-5 py-2 md:px-10 md:py-4 shadow-md rounded-xl  my-4 hover:shadow-xl active:scale-90 transition duration-150'>{buttonText}</button>
+            </div>
+        </section>
+    )
+}
+
+export default LargeCard
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,72 +4,78 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import SmallCard from '../components/SmallCard'
 import MediumCard from '../components/MediumCard'
+import LargeCard from '../components/LargeCard'
 
-function Home({ exploreData, cardsData }){
-  return (
-    <div className="">
-      <Head>
-        <title>Senior Project</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header/>
-      <Banner />
-      <main className='max-w-7xl mx-auto px-8 sm:px16 '>
-        <section className='pt-6'>
-          <h2 className='text-4xl font-semibold pb-5'>Explore Nearby</h2>
+function Home({ exploreData, cardsData }) {
+    return (
+        <div className="">
+            <Head>
+                <title>Senior Project</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <Banner />
+            <main className="max-w-7xl mx-auto px-8 sm:px16 ">
+                <section className="pt-6">
+                    <h2 className="text-4xl font-semibold pb-5">
+                        Explore Nearby
+                    </h2>
 
-          {/* Pull data from a server - API endpoints */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
-            {exploreData?.map(({img, location, distance}) => (
-              <SmallCard 
-                key={img}
-                img={img} 
-                distance={distance} 
-                location={location} 
-              />
-            ))}
-          </div>
-        </section>
-        <section>
-          <h2 className='text-4xl font-semibold py-8'>Live Anywhere</h2>
-          <div className=' flex overflow-scroll scrollbar-hide  mx-2 space-x-3 p-3 -ml-3'>
-            {cardsData.map(({img,title}) => (
-              <MediumCard
-              key={img}
-              img={img}
-              title={title}
-              />
-            ))}
-          </div>
-        </section>
-      </main>
-    </div>
-  )
+                    {/* Pull data from a server - API endpoints */}
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
+                        {exploreData?.map(({ img, location, distance }) => (
+                            <SmallCard
+                                key={img}
+                                img={img}
+                                distance={distance}
+                                location={location}
+                            />
+                        ))}
+                    </div>
+                </section>
+                <section>
+                    <h2 className="text-4xl font-semibold py-8">
+                        Live Anywhere
+                    </h2>
+                    <div className=" flex overflow-scroll scrollbar-hide  mx-2 space-x-3 p-3 -ml-3">
+                        {cardsData.map(({ img, title }) => (
+                            <MediumCard key={img} img={img} title={title} />
+                        ))}
+                    </div>
+                </section>
+
+                <LargeCard
+                    img="https://links.papareact.com/4cj"
+                    title="The Greatest Outdoors"
+                    description="Wishlists curated by Us."
+                    buttonText="Get Inspired"
+                />
+            </main>
+        </div>
+    )
 }
 // importing paths for local fetching from mockdata.json
-import fsPromises from 'fs/promises';
+import fsPromises from 'fs/promises'
 import path from 'path'
 export async function getStaticProps() {
-  // 
-  const filePath = path.join(process.cwd(), 'pages/MockData.json');
-  const jsonData = await fsPromises.readFile(filePath);
-  const exploreData = JSON.parse(jsonData);
-  // const res = await fetch('MockData.json')
-  // // const res = await fetch('https://www.jsonkeeper.com/b/4G1G')
-  // const exploreData = await res.json()
-  
-  const filePath2 = path.join(process.cwd(), 'pages/MockData2.json');
-  const jsonData2 = await fsPromises.readFile(filePath2);
-  const cardsData = JSON.parse(jsonData2);
+    //
+    const filePath = path.join(process.cwd(), 'pages/MockData.json')
+    const jsonData = await fsPromises.readFile(filePath)
+    const exploreData = JSON.parse(jsonData)
+    // const res = await fetch('MockData.json')
+    // // const res = await fetch('https://www.jsonkeeper.com/b/4G1G')
+    // const exploreData = await res.json()
 
+    const filePath2 = path.join(process.cwd(), 'pages/MockData2.json')
+    const jsonData2 = await fsPromises.readFile(filePath2)
+    const cardsData = JSON.parse(jsonData2)
 
-
-  return{
-    props: {
-      exploreData,
-      cardsData
-    },
-  }
+    return {
+        props: {
+            exploreData,
+            cardsData,
+        },
+    }
 }
 
 export default Home
